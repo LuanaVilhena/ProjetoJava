73b4c3dfe333ea504558025a80f2a636bf8126df
Luana Vilhena <129390680+LuanaVilhena@users.noreply.github.com>
2023-11-12 12:58:08 -0300

    Create CadVoluntarios.java
    Cadastro dos voluntários

---
+++ CadVoluntarios.java
@@ -0,0 +1,26 @@
+public class CadVoluntarios extends Cadastro {
+
+    private int idade;
+    private String endereco;
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+    return super.toString() + ", Tipo: Voluntário" + "Idade: " + idade + "Endereço: " + endereco;
+    }
+}
